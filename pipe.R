5d6516b7bf777701e68d642b7d2590b55abd9af1
relund (Lars Relund) <junk@relund.dk>
2009-12-10 19:47:25 +0000

    fixed alloc out of bounds on sIdS2
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/dairy@13 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pipe.R
+++ pipe.R
@@ -18,25 +18,38 @@ library(dairy)
 #mod$getFields()
 #mod$genBinaryR(prefix="genR_")
 
-mod<-HMDPDaily(maxLac=3,csvPrM="prM.csv")
+#mod<-HMDPDaily(maxLac=1,oOestus=Oestus(gestLth=30,pregTestLth=2,insemStart=2, insemFinish=4, dryPeriodLth=4), maxKL=0.9)
+mod<-HMDPDaily(maxLac=1,csvPrM="prM.csv")
+mod$genBinaryC(prefix="genCLac1_")
+
+mod$maxLac<-2
+mod$genBinaryC(prefix="genCLac2_")
+
+mod$maxLac<-3
 mod$genBinaryC(prefix="genCLac3_")
 
-#mod$maxLac<-2
-#mod$genBinaryC(prefix="genCLac2_")
-#
-#mod$maxLac<-3
-#mod$genBinaryC(prefix="genCLac3_")
+mod$maxLac<-4
+mod$genBinaryC(prefix="genCLac4_")
 
-#mod$maxLac<-4
-#mod$genBinaryC(prefix="genCLac4_")
-#
-#mod$maxLac<-5
-#mod$genBinaryC(prefix="genCLac5_")
-#
-#mod$maxLac<-10
-#mod$genBinaryC(prefix="genCLac10_")
-#rm(mod)
-#gc()
+mod$maxLac<-5
+mod$genBinaryC(prefix="genCLac5_")
+
+mod$maxLac<-6
+mod$genBinaryC(prefix="genCLac6_")
+
+mod$maxLac<-7
+mod$genBinaryC(prefix="genCLac7_")
+
+mod$maxLac<-8
+mod$genBinaryC(prefix="genCLac8_")
+
+mod$maxLac<-9
+mod$genBinaryC(prefix="genCLac9_")
+
+mod$maxLac<-10
+mod$genBinaryC(prefix="genCLac10_")
+rm(mod)
+gc()
 
 #mdp<-loadMDP(prefix="genC_")
 
